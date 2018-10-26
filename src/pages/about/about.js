22571c43087ae173847b22899460d951ca66d074
wangqingwen <wangqingwen1@100tal.com>
2018-10-26 16:45:16 +0800

    初始化项目

---
+++ src/pages/about/about.js
@@ -0,0 +1,15 @@
+/* eslint-disable */
+import './about.less';
+
+console.log('11111'+$(document.body));
+// console.log('11111'+gateWay);
+console.log(api)
+import imgUrl from '../../images/cat.jpeg';
+
+
+const img = document.createElement("img");
+img.style.width = "200px";
+img.src = imgUrl;
+document.body.appendChild(img);
+import 'bootstrap/dist/css/bootstrap.min.css'
+import 'bootstrap/dist/js/bootstrap.min.js'
